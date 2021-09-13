6f0c1d6e008c69f59b17bce96049ed65994a2668
SujoyNath1511 <59268862+SujoyNath1511@users.noreply.github.com>
2021-09-13 12:09:22 -0400

    Added debnat17 and testDebnat17 to IntroLab and relevant test in IntroLabTest.

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -33,13 +33,16 @@ public class IntroLab {
         return message_to_print + message;
     }
 
-    /* TODO: In the space below, write your our method.
-     *       The method should be named with your utorID and should call
-     *       printLabMessage with some parameters.
+    /**
+     * Returns a message greeting for Sujoy Deb Nath using the printLabMessage method
+     * with Sujoy's details.
      *
-     * As a reference, we've provided the sample method exampleStudent.
-     * Make sure you document your method properly!
+     * @return A greetings message.
      */
+    public static String debnat17(){
+        return printLabMessage("Hi", "Sujoy Deb Nath", true,
+                "Happy to be here!");
+    }
 
     /**
      * An example method that calls on printLabMessage.
@@ -53,16 +56,8 @@ public class IntroLab {
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
 
-        /* TODO: Add a call to the method you wrote in this main method,
-         *        and print the results of it.
-         *
-         * As a reference, we've provided a sample call to exampleStudent
-         * above. Afterwards: run this file to see the output!
-         */
-
+        System.out.println(debnat17());
 
     }
-
-    // TODO: Go to test/java/IntroLabTest.java afterwards and complete the TODO.
 
 }
